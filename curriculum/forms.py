159cccb79f6b272219116505026d6f404a909254
Abdellah Targaoui <abdellah.targaoui@gmail.com>
2022-02-19 16:02:32 +0100

    create templates in app_users ·

--- curriculum/forms.py
+++ curriculum/forms.py
@@ -1,21 +1,25 @@
 from django import forms
 from .models import Comment, Reply, Lesson
 
+
 class LessonForm(forms.ModelForm):
     class Meta:
         model = Lesson
-        fields = ('lesson_id','name','position','video','ppt','Notes')
+        fields = ('lesson_id', 'name', 'position', 'video', 'ppt', 'Notes')
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
 
-        labels = {"body":"Comment:"}
+        labels = {"body": "Comment:"}
 
         widgets = {
-            'body': forms.Textarea(attrs={'class':'form-control', 'rows':4, 'cols':70, 'placeholder':"Enter Your Comment"}),
+            'body': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 4, 'cols': 70, 'placeholder': "Enter Your Comment"}),
         }
+
 
 class ReplyForm(forms.ModelForm):
     class Meta:
@@ -23,7 +27,7 @@ class ReplyForm(forms.ModelForm):
         fields = ('reply_body',)
 
         widgets = {
-            'reply_body': forms.Textarea(attrs={'class':'form-control', 'rows':2, 'cols':10}),
+            'reply_body': forms.Textarea(attrs={'class': 'form-control', 'rows': 2, 'cols': 10}),
         }
 
     def __init__(self, *args, **kwargs):
